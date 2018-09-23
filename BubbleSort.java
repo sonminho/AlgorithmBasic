fdc73a3d43b498436e520fd82f66f53689ce3bc4
sonminho <30867443+sonminho@users.noreply.github.com>
2018-09-23 20:37:05 +0900

    버블소트

--- BubbleSort.java
+++ BubbleSort.java
@@ -23,11 +23,17 @@ public class BubbleSort {
 	}
 	
 	static void bubbleSort2(int[] a, int n) {
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n-i-1; j++) {
-				if(a[j] > a[j+1]) 
+		int k = n-1;
+		
+		for(int i = 0; i < n-1; i++) {
+			int last = 0;
+			for(int j = 0; j < k; j++) {
+				if(a[j] > a[j+1]) { 
 					swap(a, j, j+1);
+					last = j;
+				}
 			}
+			k = last;
 		}
 	}
 	
