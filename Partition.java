cf39358b7265078affea31339cd632c630853ab9
sonminho <30867443+sonminho@users.noreply.github.com>
2018-09-26 16:25:21 +0900

    분할

--- Partition.java
+++ Partition.java
@@ -1,48 +1,62 @@
+
 import java.util.Scanner;
 
-public class QuickSort {
+public class Partition {
 	static void swap(int[] a, int idx1, int idx2) {
 		int temp = a[idx1];
 		a[idx1] = a[idx2];
 		a[idx2] = temp;
 	}
 	
-	static void quickSort(int[] a, int left, int right) {
-		int pl = left;
-		int pr = right;
-		int x = a[(pl+pr)/2]; // 피벗
+	static void partition(int[] a, int n) {
+		int pl = 0;
+		int pr = n - 1;
+		int x = a[n/2];  // 피벗
 		
-		do{
+		do {
 			while(a[pl] < x) pl++;
 			while(a[pr] > x) pr--;
 			
 			if(pl <= pr)
 				swap(a, pl++, pr--);
-		}while(pl <= pr);
+		} while(pl <= pr);
 		
-		if(left < pr) quickSort(a, left, pr);
-		if(pl < right) quickSort(a, pl, right);
+		System.out.println("피벗 값은 " + x + "입니다.");
+		
+		System.out.println("피벗 이하의 그룹");
+		for(int i = 0; i <= pl-1; i++) {
+			System.out.print(a[i] + " ");
+		}
+		System.out.println();
+		
+		if(pl > pr+1) {
+			System.out.println("피벗과 일치하는 그룹");
+			for(int i = pr+1; i <= pl-1; i++) {
+				System.out.print(a[i] + " ");
+			}
+		}
+		System.out.println();
+		
+		System.out.println("피벗 이상의 그룹");
+		for(int i = pr+1; i < n; i++) {
+			System.out.print(a[i] + " ");
+		}
+		System.out.println();
 	}
 	
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		Scanner stdIn = new Scanner(System.in);
 		
-		System.out.println("퀵 정렬");
-		System.out.print("요솟 수 ? ");
+		System.out.println("배열을 나눕니다.");
+		System.out.print("요솟 수 : ");
 		
-		int n = sc.nextInt();
-		int[] a = new int[n];
+		int n = stdIn.nextInt();
+		int[] x=  new int[n];
 		
 		for(int i = 0; i < n; i++) {
-			System.out.print("a[" + i + "] : ");
-			a[i] = sc.nextInt();
+			System.out.print("x[" + i + "] = ");
+			x[i] = stdIn.nextInt();
 		}
-		
-		quickSort(a, 0, n-1);
-		
-		System.out.println("오름차순 결과");
-		for(int i = 0; i < n; i++) {
-			System.out.print(a[i] + " "); 
-		}
+		partition(x, n);
 	}
 }
