b0b290a7637f7c2f9d375eb769f6f7df0e52bf82
sonminho <30867443+sonminho@users.noreply.github.com>
2018-12-19 21:49:49 +0900

    워드카운터

---
+++ JAVA_PRACTICE/WORDCOUNTER/WordCounter.java
@@ -0,0 +1,56 @@
+﻿
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class WordCounter {
+	Map<String, Integer> map;
+	List<String> list;
+	
+	WordCounter(List<String> list) {
+		super();
+		this.map = new HashMap<String, Integer>();
+		this.list = list;
+	}
+	
+	public void parse() {
+		for(String line : list) {
+			StringTokenizer stk = new StringTokenizer(line, " \t,\"'.?!");
+			
+			while(stk.hasMoreTokens()) {
+				String token = stk.nextToken();
+				
+				// 처음등장했으면
+				if(!map.containsKey(token)) {
+					map.put(token, 1);
+				} else {
+					int count = map.get(token);
+					count++;
+					map.put(token, count);
+				}
+			}
+		}
+	}
+	
+	public void printResult(PrintStream out) {
+		for(String key : map.keySet()) {
+			out.println(key +  " : " + map.get(key));
+		}
+	}
+	
+	public void printResult(PrintStream out, Comparator<Map.Entry<String, Integer>> comparator) {
+		// Map.Entry 만들기
+		List<Map.Entry<String, Integer>> entries = new ArrayList<>(map.entrySet());
+		
+		Collections.sort(entries, comparator);
+		
+		for(Map.Entry<String, Integer> entry : entries) {
+			out.println(entry.getKey() + " : " + entry.getValue());
+		}
+	}
+}
---
+++ JAVA_PRACTICE/WORDCOUNTER/WordCounterExample.java
@@ -0,0 +1,59 @@
+﻿
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class WordCounterExample {
+	public static void main(String[] args) {
+		File file = new File("C:/Temp/test.txt");
+		
+		try {
+			//List<String> lines = readLines(file);
+			List<String> lines = Files.readAllLines(file.toPath());
+			WordCounter wc = new WordCounter(lines);
+			wc.parse();
+			
+			// 단어 철자순
+			wc.printResult(System.out, new Comparator<Map.Entry<String, Integer>>() {
+				@Override
+				public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
+					return o1.getKey().compareTo(o2.getKey());
+				}
+				
+			});
+			
+			System.out.println("\n단어의 출연 빈도순(내림차순)\n");
+			wc.printResult(System.out, new Comparator<Map.Entry<String, Integer>>(){
+				@Override
+				public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
+					return o2.getValue().compareTo(o1.getValue());
+				}
+			});
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static List<String> readLines(File file) throws Exception {
+		 List<String> list = new ArrayList<>();
+		 
+		 try(
+		     FileReader fr = new FileReader(file);
+		     BufferedReader br = new BufferedReader(fr);
+		 ) {
+			 String line;
+			 while((line = br.readLine()) != null) {
+				 list.add(line);
+			 }
+		 } catch(Exception e){
+			 e.printStackTrace();
+		 }
+		 return list;
+	 }
+}
